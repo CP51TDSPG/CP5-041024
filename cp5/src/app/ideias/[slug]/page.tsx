d430017eba9d3d7dc5999423b60d3669d30ad459
ludanii <ludanielle2005@hotmail.com>
2024-10-04 14:45:15 -0300

    Importando as imagens da NASA.

--- cp5/src/app/ideias/[slug]/page.tsx
+++ cp5/src/app/ideias/[slug]/page.tsx
@@ -3,17 +3,17 @@ import Image from "next/image";
 const ideas = {
     '1': {
       description: 'A primeira ideia fala sobre a catástrofe global e sua relação com a aproximação de Vênus.',
-      imageUrl: '/images/venus-catastrophe.jpg', // Coloque a imagem na pasta public/images
+      imageUrl: 'https://images-assets.nasa.gov/image/PIA23780/PIA23780~medium.jpg',
       altText: 'Representação da catástrofe global relacionada a Vênus'
     },
     '2': {
       description: 'A segunda ideia propõe que eventos astronômicos estão conectados a mitos antigos.',
-      imageUrl: '/images/astronomical-myths.jpg', // Coloque a imagem na pasta public/images
+      imageUrl: 'https://images-assets.nasa.gov/image/PIA00342/PIA00342~medium.jpg',
       altText: 'Eventos astronômicos relacionados a mitos antigos'
     },
     '3': {
       description: 'A terceira ideia discute o impacto de eventos cósmicos na civilização humana.',
-      imageUrl: '/images/cosmic-impact.jpg', // Coloque a imagem na pasta public/images
+      imageUrl: 'https://images-assets.nasa.gov/image/PIA00003/PIA00003~medium.jpg',
       altText: 'Impacto de eventos cósmicos na civilização humana'
     }
   };
