3c41610e9322e5dff8ab1c98bb269ff5d099aadd
ludanii <ludanielle2005@hotmail.com>
2024-10-04 14:26:31 -0300

    Inicializando o layout.

--- cp5/src/app/layout.tsx
+++ cp5/src/app/layout.tsx
@@ -1,12 +1,17 @@
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+import Rodape from "./components/Rodape/Rodape";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body>
+        <Cabecalho />
         {children}
+        <Rodape />
       </body>
     </html>
   );
